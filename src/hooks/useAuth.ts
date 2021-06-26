786175d0369afb8c786721a1ca26ee39a067e79f
Gabriel Vieira <gabrielvieirasousa2001@gmail.com>
2021-06-26 14:08:02 -0300

    finalizado

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,8 @@
+import { useContext } from "react";
+import { AuthContext } from "../Context/AuthContext";
+
+export function useAuth() {
+  const value = useContext(AuthContext);
+
+  return value;
+}
